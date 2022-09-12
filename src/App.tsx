fdab0586981b7c56e70b407be23ee623dc8913b3
Aleksey Gusev <audetv@gmail.com>
2022-09-12 21:26:48 +0300

    Added bootstrap example login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <main className="form-signin w-100 m-auto">
+          <form>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <input type="email" className="form-control" placeholder="name@example.com" />
+            <input type="password" className="form-control" placeholder="Password" />
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          </form>
+        </main>
+      </div>
   );
 }
 
