265176ff851353d1bd4aee6793cfe1c31a05d733
Aleksey Gusev <audetv@gmail.com>
2022-09-14 09:31:31 +0300

    Moved fetching user to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Nav from './components/Nav';
@@ -7,13 +7,29 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 
 function App() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    (
+        async () => {
+          const response = await fetch('http://localhost:8000/api/user', {
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+          })
+
+          const content = await response.json()
+          setName(content.name)
+        }
+    )();
+  })
+
   return (
       <div className="App">
         <BrowserRouter>
           <Nav />
           <main className="form-signin w-100 m-auto">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home name={name} />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
             </Routes>
